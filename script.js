00dc386adbd04ceedaf758a0d70a6b95c09bda08
MathiasCervantes <mcervantesportocarrero@gmail.com>
2024-11-30 10:50:25 -0500

    Cambios

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onload=function(){
+    alert("Bienvenidos xd");
+};
+
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior:'smooth'
+        });
+    });
+});
